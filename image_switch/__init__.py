deef8af433e799887599f723ffa68ff259cd7884
Doug Hogan <doug@doughogan.com>
2026-03-26 10:26:04 -0500

    Add files via upload

--- image_switch/__init__.py
+++ image_switch/__init__.py
@@ -1,5 +1,5 @@
 import os
-from .image_switch import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .switch_which import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 WEB_DIRECTORY = os.path.join(os.path.dirname(__file__), "js")
 
